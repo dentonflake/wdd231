449762ba8306f65a412d554bdc5b85689490e174
Denton Flake <me@dentonflake.com>
2025-08-03 15:27:47 -0700

    Started final project

--- final/scripts/dates.js
+++
@@ -1,6 +0,0 @@
-
-const yearElement = document.getElementById('currentyear');
-yearElement.textContent = new Date().getFullYear().toString();
-
-const lastModified = document.getElementById('lastModified');
-lastModified.textContent = `Last Modification: ${document.lastModified}`;
