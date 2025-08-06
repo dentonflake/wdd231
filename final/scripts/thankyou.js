cc6cf1a1413f0c9a65eaa4396b1032d4eb493df5
Denton Flake <me@dentonflake.com>
2025-08-05 22:19:56 -0700

    Added thank you page content

---
+++ final/scripts/thankyou.js
@@ -0,0 +1,42 @@
+
+const getQueryParams = () => {
+  return Object.fromEntries(new URLSearchParams(window.location.search))
+}
+
+const validateRequiredFields = (queryParameters) => {
+
+  const requiredFields = [
+    "name",
+    "phone",
+    "message"
+  ]
+
+  return requiredFields.every(key => queryParameters[key] && queryParameters[key] !== "")
+}
+
+const formatPhoneNumber = (phone) => {
+
+  const match = phone.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  if (match) {
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
+  }
+
+  return phone;
+};
+
+const queryParameters = getQueryParams()
+
+if (!validateRequiredFields(queryParameters)) {
+  window.location.href = "contact.html";
+}
+
+const {
+  name,
+  phone,
+  message
+} = queryParameters
+
+document.getElementById('name').textContent = `${name}`;
+document.getElementById('phone').textContent = formatPhoneNumber(phone);
+document.getElementById('message').textContent = `${message}`;
